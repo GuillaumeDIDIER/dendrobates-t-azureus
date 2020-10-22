a331fdd76e831af2fb8facf941e9eadc1174706a
Guillume DIDIER <guillaume.didier@inria.fr>
2020-10-22 14:49:09 +0200

    Add a crate meant to one day factorize some common code to timing side channels

---
+++ basic_timing_cache_channel/src/lib.rs
@@ -0,0 +1,15 @@
+// TODO
+
+// Common logic for the ability to calibrate along slices
+// Core issues should be orthogonal
+// Extend to multithread ?
+
+// Should be used by F+F and non Naive F+R
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
