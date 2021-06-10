b9980aa67b1ad66f058a401063edda0ea931d18b
Guillume DIDIER <guillaume.didier@inria.fr>
2021-06-10 11:22:21 +0200

    Rust nightly update - FF & FR

--- flush_flush/src/lib.rs
+++ flush_flush/src/lib.rs
@@ -7,6 +7,7 @@ use basic_timing_cache_channel::{
     SingleChannel, TimingChannelPrimitives, TopologyAwareTimingChannel,
 };
 
+use cache_side_channel::MultipleAddrCacheSideChannel;
 use cache_utils::calibration::only_flush;
 
 #[derive(Debug)]
@@ -19,6 +20,8 @@ impl TimingChannelPrimitives for FFPrimitives {
 }
 
 pub type FlushAndFlush = TopologyAwareTimingChannel<FFPrimitives>;
+
+pub type FFHandle = <FlushAndFlush as MultipleAddrCacheSideChannel>::Handle;
 
 pub type SingleFlushAndFlush = SingleChannel<FlushAndFlush>;
 
