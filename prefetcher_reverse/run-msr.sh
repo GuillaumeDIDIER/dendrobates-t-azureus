c12a3ba29bd348a1da898f30ee5b9c59f2104ce9
Guillaume DIDIER <guillaume.didier@inria.fr>
2021-08-02 15:53:22 +0200

    Fix missing --bin in cargo invocation

--- prefetcher_reverse/run-msr.sh
+++ prefetcher_reverse/run-msr.sh
@@ -2,6 +2,6 @@
 PREFETCH_MSR=$1
 sudo wrmsr -a 0x1a4 $PREFETCH_MSR
 sudo rdmsr -a 0x1a4
-cargo run --release > with-${PREFETCH_MSR}-prefetcher.log
+cargo run --bin prefetcher_reverse --release > with-${PREFETCH_MSR}-prefetcher.log
 
 
