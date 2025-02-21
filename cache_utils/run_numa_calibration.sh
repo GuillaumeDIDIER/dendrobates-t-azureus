9a232b5c92a2aebd53ec29784e32e0b0daf31ea0
Guillaume Didier <gdidier@ftoulouse.toulouse.grid5000.fr>
2025-02-21 13:39:37 +0100

    Scripts

--- cache_utils/run_numa_calibration.sh
+++ cache_utils/run_numa_calibration.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "$0"
 abs_self=`realpath "$0"`
@@ -6,13 +6,23 @@ echo $abs_self
 cache_utils=`dirname "$abs_self"`
 echo $cache_utils
 
+#pushd $cache_utils
+#cargo build --release --bin numa_calibration
+#popd
+
 mkdir -p /tmp/numa_cal
 pushd /tmp/numa_cal
 
-echo $cache_utils/../target/release/numa_calibration > log.txt 2> err.txt
+sudo-g5k cpupower frequency-set -g performance
+
+sudo-g5k sh -c "echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo"
+
+$cache_utils/../target/release/numa_calibration > log.txt 2> err.txt
 xz *.txt
 
 popd
 
 cp *.xz ./
 rm -Rf /tmp/numa_cal
+
+
---
+++ cache_utils/setup.sh
@@ -0,0 +1,6 @@
+sudo-g5k apt-get update
+sudo-g5k apt-get install libnuma-dev curl git bzip2 gcc hwloc htop libclang-dev libcpufreq-dev -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+bash -c 'source "$HOME/.cargo/env" && rustup toolchain install nightly --profile complete --component cargo && rustup default nightly'
+cd ~/dendrobates-t-azureus/cache_utils/
+cargo build --release --bin numa_calibration
