ed5118fcc2801c7e2f34534661108fda72e40cb0
Guillaume DIDIER <guillaume.didier.2014@polytechnique.org>
2025-02-26 14:11:38 +0100

    Refactor run scripts

---
+++ cache_utils/run_numa_calibration-2-20.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo "$0"
+abs_self=`realpath "$0"`
+echo $abs_self
+cache_utils=`dirname "$abs_self"`
+echo $cache_utils
+
+#pushd $cache_utils
+#cargo build --release --bin numa_calibration
+#popd
+
+sudo-g5k apt install msr-tools
+sudo-g5k modprobe msr
+
+
+mkdir -p /tmp/numa_cal_variable
+pushd /tmp/numa_cal_variable
+
+sudo-g5k sh -c "echo 0 > /proc/sys/kernel/numa_balancing"
+
+$cache_utils/../target/release/numa_calibration > log.txt 2> err.txt
+
+sudo-g5k sh -c "echo 1 > /proc/sys/kernel/numa_balancing"
+
+xz *.txt
+
+popd
+
+mkdir ./variable_freq
+cp *.xz ./variable_freq/
+rm -Rf /tmp/numa_cal_variable
+
+mkdir -p /tmp/numa_cal_fixed
+pushd /tmp/numa_cal_fixed
+
+sudo modprobe wrmsr -a 420 0xf
+
+sudo-g5k cpupower frequency-set -g performance
+sudo-g5k sh -c "echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo"
+sudo-g5k sh -c "echo 0 > /proc/sys/kernel/numa_balancing"
+
+$cache_utils/../target/release/numa_calibration > log.txt 2> err.txt
+
+sudo-g5k sh -c "echo 0 > /sys/devices/system/cpu/intel_pstate/no_turbo"
+sudo-g5k sh -c "echo 1 > /proc/sys/kernel/numa_balancing"
+sudo modprobe wrmsr -a 420 0
+
+xz *.txt
+
+popd
+
+
+mkdir ./fixed_freq
+cp *.xz ./fixed_freq/
+rm -Rf /tmp/numa_cal_fixed
+
+
